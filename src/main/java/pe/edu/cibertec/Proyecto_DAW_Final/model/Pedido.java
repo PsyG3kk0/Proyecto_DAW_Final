15d457f777c16067fcd35576d7317c3d0af4d8db
Adexis2004 <junpilco12@gmail.com>
2024-12-22 11:28:23 -0500

    Completando Model Pedido

---
+++ src/main/java/pe/edu/cibertec/Proyecto_DAW_Final/model/Pedido.java
@@ -0,0 +1,19 @@
+package pe.edu.cibertec.Proyecto_DAW_Final.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Pedido {
+	private Integer cantidad;
+	private Integer productoId;
+	
+
+}
