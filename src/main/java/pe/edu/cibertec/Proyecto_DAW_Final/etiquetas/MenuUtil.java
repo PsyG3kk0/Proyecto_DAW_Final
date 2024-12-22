6924df45c12d18581312b950c3c1a6932dcbd480
Adexis2004 <junpilco12@gmail.com>
2024-12-22 11:35:40 -0500

    Completando el MenuUtil

--- src/main/java/pe/edu/cibertec/Proyecto_DAW_Final/etiquetas/MenuUtil.java
+++ src/main/java/pe/edu/cibertec/Proyecto_DAW_Final/etiquetas/MenuUtil.java
@@ -1,5 +1,37 @@
 package pe.edu.cibertec.Proyecto_DAW_Final.etiquetas;
 
-public class MenuUtil {
+import pe.edu.cibertec.Proyecto_DAW_Final.entity.UsuarioEntity;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Component;
 
+@Component
+public class MenuUtil {
+	public String generarMenu(UsuarioEntity usuarioEntity, Integer tipoUsuario, HttpSession sesion) {
+        StringBuilder menu = new StringBuilder();
+
+        menu.append("<nav class='navbar navbar-expand-lg navbar-light bg-light'>")
+            .append("<button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>")
+            .append("<span class='navbar-toggler-icon'></span>")
+            .append("</button>")
+            .append("<div class='collapse navbar-collapse' id='navbarNav'>")
+            .append("<ul class='navbar-nav'>");
+
+        if (tipoUsuario == 1) {
+            menu.append("<li class='nav-item'><a class='nav-link' href='/agregar_bocadito'>Mantenimiento Bocadito</a></li>")
+                .append("<li class='nav-item'><a class='nav-link' href='/agregar_torta'>Mantenimiento Torta</a></li>")
+                .append("<li class='nav-item'><a class='nav-link' href='/agregar_usuario'>Mantenimiento Usuarios</a></li>")
+                .append("<li class='nav-item'><a class='nav-link' href='/menu'>Ver tienda</a></li>");
+        } else if (tipoUsuario == 2) {
+            menu.append("<li class='nav-item'><a class='nav-link' href='/menuHtml'>Inicio</a></li>")
+                .append("<li class='nav-item'><a class='nav-link' href='/menu'>Menu</a></li>")
+                .append("<li class='nav-item'><a class='nav-link' href='/tienda'>Tienda</a></li>")
+                .append("<li class='nav-item'><a class='nav-link' href='/nosotroHtml'>Nosotros</a></li>");
+        }
+
+        menu.append("</ul>")
+            .append("</div>")
+            .append("</nav>");
+
+        return menu.toString();
+    }
 }
