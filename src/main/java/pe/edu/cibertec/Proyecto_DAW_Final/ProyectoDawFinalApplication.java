14c9c06795fadd2965303b84247867ae8898d38a
David Chavez <jose_17298@hotmail.com>
2024-12-22 09:11:06 -0500

    Creando Proyecto en Git

---
+++ src/main/java/pe/edu/cibertec/Proyecto_DAW_Final/ProyectoDawFinalApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Proyecto_DAW_Final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDawFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDawFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/Proyecto_DAW_Final/ProyectoDawFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Proyecto_DAW_Final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDawFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
